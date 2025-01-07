76521f8367f1a5ed91e5e0a344b123f96cf4e888
DIYOR18 <DIXON.MUNOZ@ucr.ac.cr>
2025-01-06 21:40:41 -0600

    arreglando cosas de berny

--- src/components/MenMenuList/ConfigurationMenu.js
+++ src/components/MenMenuList/ConfigurationMenu.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom'; 
+import { useLocation, useNavigate } from 'react-router-dom';
 import api from '../../components/Auth/AxiosConfig';
 import Swal from 'sweetalert2';
 import './ConfigurationMenu.css';
@@ -10,13 +10,14 @@ function ConfigurationMenu() {
   const [selectedCategory, setSelectedCategory] = useState('');
   const [selectedSubCategory, setSelectedSubCategory] = useState('');
   const [editingDish, setEditingDish] = useState(null);
-  
+  const [imagePreview, setImagePreview] = useState(null);
+
   const navigate = useNavigate();
-  const location = useLocation(); 
-  const { dish } = location.state || {}; 
+  const location = useLocation();
+  const { dish } = location.state || {};
 
   useEffect(() => {
-    fetchCategories(); 
+    fetchCategories();
     if (dish) {
       setEditingDish(dish);
       setSelectedCategory(dish.idCategory);
@@ -26,7 +27,7 @@ function ConfigurationMenu() {
 
   useEffect(() => {
     if (selectedCategory) {
-      fetchSubCategories(selectedCategory); 
+      fetchSubCategories(selectedCategory);
     }
   }, [selectedCategory]);
 
@@ -54,12 +55,12 @@ function ConfigurationMenu() {
     const description = event.target.description.value;
     const price = event.target.price.value;
     const image = event.target.image.files[0];
-  
+
     if (!selectedCategory || !selectedSubCategory) {
       Swal.fire('Error', 'Por favor selecciona una categoría y subcategoría.', 'error');
       return;
     }
-  
+
     const formData = new FormData();
     formData.append('name', name);
     formData.append('description', description);
@@ -67,11 +68,9 @@ function ConfigurationMenu() {
     formData.append('idCategory', selectedCategory);
     formData.append('idSubCategory', selectedSubCategory);
     if (image) formData.append('image', image);
-  
+
     try {
-      // Verificar si se está editando un platillo
       if (editingDish) {
-        // Mostrar alerta de confirmación antes de actualizar
         const result = await Swal.fire({
           title: '¿Estás seguro?',
           text: "¿Deseas actualizar este platillo?",
@@ -80,115 +79,130 @@ function ConfigurationMenu() {
           confirmButtonText: 'Sí, actualizar',
           cancelButtonText: 'Cancelar',
         });
-  
-        // Si el usuario confirma la actualización
+
         if (result.isConfirmed) {
           await api.put(`/dish/update/${editingDish.idDish}`, formData);
           Swal.fire('Éxito', 'Platillo actualizado correctamente.', 'success');
-          setEditingDish(null);  // Resetear el platillo en edición
-          // Redirigir al menú de platillos después de guardar
-          navigate('/menu');  // Puedes modificar esto según tu ruta deseada
+          setEditingDish(null);
+          navigate('/menu');
         }
       } else {
-        // Si no se está editando, agregar un nuevo platillo
         await api.post('/dish/add', formData);
         Swal.fire('Éxito', 'Platillo agregado correctamente.', 'success');
-        // Limpiar el formulario después de agregar
         setSelectedCategory('');
         setSelectedSubCategory('');
-        event.target.reset();  // Limpiar el formulario
+        event.target.reset();
       }
     } catch (error) {
       console.error('Error al guardar el platillo:', error);
       Swal.fire('Error', 'No se pudo guardar el platillo.', 'error');
     }
   };
-  
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setImagePreview(null);
+    }
+  };
 
   return (
     <section className="configuration_menu_section">
       <form onSubmit={handleSubmit} className="centered-form">
-      <h3 className="centered-title">Bienvenido a la sección de ajustes de menú, aquí podrás agregar un nuevo platillo</h3>
-        <div className="form-group">
-          <label htmlFor="name">Nombre</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="form-control"
-            placeholder="Ingresa el nombre del platillo"
-            defaultValue={editingDish ? editingDish.name : ''}
-            required
-          />
+        <div className="left-column">
+          <h3 className="centered-title">Bienvenido a la sección de ajustes de menú, aquí podrás agregar un nuevo platillo</h3>
+          <div className="form-group">
+            <label htmlFor="name">Nombre</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="form-control"
+              placeholder="Ingresa el nombre del platillo"
+              defaultValue={editingDish ? editingDish.name : ''}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="description">Descripción</label>
+            <textarea
+              id="description"
+              name="description"
+              className="form-control"
+              placeholder="Ingresa la descripción del platillo"
+              defaultValue={editingDish ? editingDish.description : ''}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="price">Precio</label>
+            <input
+              type="number"
+              id="price"
+              name="price"
+              className="form-control"
+              placeholder="Ingresa el precio del platillo"
+              defaultValue={editingDish ? editingDish.price : ''}
+              required
+            />
+          </div>
+
+          <div className="categories-container">
+            <div className="form-group">
+              <label htmlFor="category">Categoría</label>
+              <select
+                id="category"
+                name="category"
+                className="form-control"
+                value={selectedCategory}
+                onChange={(e) => {
+                  setSelectedCategory(e.target.value);
+                  setSelectedSubCategory('');
+                }}
+              >
+                <option value="">Seleccionar categoría</option>
+                {categories.map((category) => (
+                  <option key={category.idCategory} value={category.idCategory}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="subcategory">Subcategoría</label>
+              <select
+                id="subcategory"
+                name="subcategory"
+                className="form-control"
+                value={selectedSubCategory}
+                onChange={(e) => setSelectedSubCategory(e.target.value)}
+              >
+                <option value="">Seleccionar subcategoría</option>
+                {subCategories.map((subCategory) => (
+                  <option key={subCategory.idSubCategory} value={subCategory.idSubCategory}>
+                    {subCategory.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
         </div>
 
-        <div className="form-group">
-          <label htmlFor="description">Descripción</label>
-          <textarea
-            id="description"
-            name="description"
-            className="form-control"
-            placeholder="Ingresa la descripción del platillo"
-            defaultValue={editingDish ? editingDish.description : ''}
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="price">Precio</label>
-          <input
-            type="number"
-            id="price"
-            name="price"
-            className="form-control"
-            placeholder="Ingresa el precio del platillo"
-            defaultValue={editingDish ? editingDish.price : ''}
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="category">Categoría</label>
-          <select
-            id="category"
-            name="category"
-            className="form-control"
-            value={selectedCategory}
-            onChange={(e) => {
-              setSelectedCategory(e.target.value);
-              setSelectedSubCategory('');
-            }}
-          >
-            <option value="">Seleccionar categoría</option>
-            {categories.map((category) => (
-              <option key={category.idCategory} value={category.idCategory}>
-                {category.name}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="subcategory">Subcategoría</label>
-          <select
-            id="subcategory"
-            name="subcategory"
-            className="form-control"
-            value={selectedSubCategory}
-            onChange={(e) => setSelectedSubCategory(e.target.value)}
-          >
-            <option value="">Seleccionar subcategoría</option>
-            {subCategories.map((subCategory) => (
-              <option key={subCategory.idSubCategory} value={subCategory.idSubCategory}>
-                {subCategory.name}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="image">Imagen</label>
-          <input type="file" id="image" name="image" className="form-control" />
+        <div className="right-column">
+          <div className="form-group">
+            <label htmlFor="image">Imagen</label>
+            <input type="file" id="image" name="image" className="form-control" onChange={handleImageChange} />
+          </div>
+          {imagePreview && <img src={imagePreview} alt="Vista previa" className="image-preview" />}
         </div>
 
         <button type="submit" className="btn btn-primary">
